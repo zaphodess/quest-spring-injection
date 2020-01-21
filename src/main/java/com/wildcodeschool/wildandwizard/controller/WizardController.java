c186c3b45f4942a4514b1a367135b6f07c6a46ca
zaphodess <57356368+zaphodess@users.noreply.github.com>
2020-01-21 15:36:19 +0100

    Update WizardController.java

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -4,6 +4,7 @@ import com.wildcodeschool.wildandwizard.entity.Wizard;
 import com.wildcodeschool.wildandwizard.repository.WizardRepository;
 import com.wildcodeschool.wildandwizard.repository.WizardDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
